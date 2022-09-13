f1f761bc8125c8b974c0afeea43c2c4a18dc8637
Serkan Polat <pserkan253@gmail.com>
2022-09-13 14:58:27 +0300

    .

---
+++ Spelling Correction with Python/Spelling Correction with Python .py
@@ -0,0 +1,13 @@
+from textblob import TextBlob
+def Convert(string):
+    li = list(string.split())
+    return li
+str1 = input("Enter your word : ")
+words=Convert(str1)
+corrected_words = []
+for i in words:
+    corrected_words.append(TextBlob(i))
+print("Wrong words :", words)
+print("Corrected Words are :")
+for i in corrected_words:
+    print(i.correct(), end=" ")
